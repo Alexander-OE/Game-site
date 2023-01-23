2d4fa68a42df809d36fe5c3536cbb09d754060f9
Egbe Alexander <alexsucceeds@gmail.com>
2023-01-23 18:03:34 +0100

    made about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import SharedLayout from "./Components/MainPage/SharedLayout";
 import LoadingSpinner from "./Components/Loader/LoadingSpinner";
+import About from "./Components/MainPage/About";
 // import HomePage from "./Components/Pages/HomePage";
 
 import React, {Suspense} from "react";
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element={<SharedLayout />} >
       
       <Route  index element={<LazyHome />}/>
+      <Route path='about' element={<About />} />
 </Route>
 
     </Routes>
